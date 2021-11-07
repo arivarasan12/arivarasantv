cdf31b89c348b2ee00dee7ef1cc7f29a43676c72
arivarasan12 <arivarasan425@gmail.com>
2021-11-07 16:24:29 +0530

    auto

--- all_serial/suntv_v2.py
+++ all_serial/suntv_v2.py
@@ -6,7 +6,7 @@ links=[]
 link=["https://www.thiraitwo.com/v/sun/0811abi/720/playlist.m3u8","https://www.thiraitwo.com/v/sun/abi0611/380/playlist.m3u8","https://www.thiraisix.com/v/vijay/0811/720/playlist.m3u8","http://www.thiraione.com/v/vijay/0811ps/hd/playlist.m3u8","https://www.thiraitwo.com/v/vijay/0811ps/720/playlist.m3u8","https://www.thiraisix.com/v/vijay/0811ps/720/playlist.m3u8","https://www.thiraisix.com/v/vijay/0811ps/hd/playlist.m3u8","https://www.thiraisix.com/v/sun/ct0611/720/playlist.m3u8","https://www.thiraisix.com/v/sun/0611kay/720/playlist.m3u8","https://www.thiraifour.com/v/sun/0611abi/720/playlist.m3u8","https://www.thiraisix.com/v/sun/0611abi/720/playlist.m3u8","https://www.thiraisix.com/v/sun/ct1026/720/playlist.m3u8","https://www.thiraitwo.com/v/sun/0611ct/720/playlist.m3u8","https://www.thiraitwo.com/v/sun/ct0611/720/playlist.m3u8","https://www.thiraisix.com/v/sun/pu1106/720/playlist.m3u8","https://www.thiraisix.com/v/sun/0611ct/720/playlist.m3u8","https://www.thiraitwo.com/v/sun/0611kay/480/playlist.m3u8","https://www.thiraisix.com/v/zee/ct1026/720/playlist.m3u8","https://cdn.tamilbliss.com/v/sun/0611ct/720/playlist.m3u8","https://www.thiraisix.com/v/sun/0611abi/480/playlist.m3u8","https://www.thiraifour.com/v/sun/0611sun/720/playlist.m3u8","https://www.thiraisix.com/v/sun/0611sun/720/playlist.m3u8","https://www.thiraitwo.com/v/sun/0611sun/720/playlist.m3u8","https://www.thiraitwo.com/v/sun/0611vp/720/playlist.m3u8","https://www.thiraitwo.com/v/sun/0611vp1/720/playlist.m3u8","https://www.thiraisix.com/v/sun/0611vp/720/playlist.m3u8","https://www.thiraifour.com/v/sun/0611vp/240/playlist.m3u8","https://www.thiraisix.com/v/sun/1026pu/720/playlist.m3u8","https://www.thiraitwo.com/v/sun/0611pu/720/playlist.m3u8","https://www.thiraitwo.com/v/sun/pu0611/720/playlist.m3u8","https://www.thiraisix.com/v/sun/0611pu/720/playlist.m3u8","https://www.thiraisix.com/v/sun/pu0611/720/playlist.m3u8","https://cdn.tamilbliss.com:443/v/sun/0611aru/720/playlist.m3u8","https://www.thiraifour.com/v/sun/0611aru/720/playlist.m3u8"]
 for a in link:
  try:
-   urllib.request.urlretrieve(a,'video_name.m3u8') 
+   urllib.request.urlretrieve(a,"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\video_name.m3u8") 
  except HTTPError or tiimeout :
   print("not:",a)
  else:
@@ -15,7 +15,7 @@ for a in link:
    links.append(a)
 for j in links:
   print(j)
-f = open("suntvserial_v2.m3u","w")
+f = open("C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\suntvserial_v2.m3u","w")
 f.write("#EXTM3U\n")
 keys = ["0811","0611sun","aru", "ct", "abi", "vp", "pu", "kay","Roja"]
 for i in links:
@@ -28,16 +28,16 @@ for i in links:
         f.write('#EXTINF:-1 tvg-logo="https://github.com/arivarasan12/arivarasantv/blob/99682071a6d6cab85d7290fda4e100e03fdb78eb/all_serial/Images/abhium_nanum.png?raw=true" group-title="SERIAL",ABHIYUM NANUM\n')
         f.write(i)
         f.write('\n')
-        urllib.request.urlretrieve(i,'abi.m3u8') 
+        urllib.request.urlretrieve(i,"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\abi.m3u8") 
         with open(r'video_name.m3u8', 'r') as file:
          data = file.read()
          data = data.replace("fileSequence",i+"fileSequence")
-        with open(r'abi.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\abi.m3u8",'w') as file:
 	        file.write(data)
-        with open(r'abi.m3u8', 'r') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\abi.m3u8", 'r') as file:
          data = file.read()
          data = data.replace("playlist.m3u8","")
-        with open(r'abi.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\abi.m3u8",'w') as file:
 	        file.write(data)
         
         print("psText replaced")
@@ -50,16 +50,16 @@ for i in links:
         f.write('#EXTINF:-1 tvg-logo="https://github.com/arivarasan12/arivarasantv/blob/7fe542a265f3fef7ece784ed6e984c90d8a74909/all_serial/Images/sundari.png?raw=true" group-title="SERIAL",SUNDARI\n')
         f.write(i)
         f.write('\n')
-        urllib.request.urlretrieve(i,'sun.m3u8') 
-        with open(r'sun.m3u8', 'r') as file:
+        urllib.request.urlretrieve(i,"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\sun.m3u8") 
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\sun.m3u8", 'r') as file:
          data = file.read()
          data = data.replace("fileSequence",i+"fileSequence")
-        with open(r'sun.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\sun.m3u8",'w') as file:
 	        file.write(data)
-        with open(r'sun.m3u8', 'r') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\sun.m3u8", 'r') as file:
          data = file.read()
          data = data.replace("playlist.m3u8","")
-        with open(r'sun.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\sun.m3u8",'w') as file:
 	        file.write(data)
         
         print("psText replaced")
@@ -67,16 +67,16 @@ for i in links:
         f.write('#EXTINF:-1 tvg-logo="https://github.com/arivarasan12/arivarasantv/blob/0269331533079bbe982e472bc8f4f7da8ea73184/all_serial/Images/chithi2.png?raw=true" group-title="SERIAL",CHITTHI 2\n')
         f.write(i)
         f.write('\n')
-        urllib.request.urlretrieve(i,'ct.m3u8') 
-        with open(r'ct.m3u8', 'r') as file:
+        urllib.request.urlretrieve(i,"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\ct.m3u8") 
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\ct.m3u8", 'r') as file:
          data = file.read()
          data = data.replace("fileSequence",i+"fileSequence")
-        with open(r'ct.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\ct.m3u8",'w') as file:
 	        file.write(data)
-        with open(r'ct.m3u8', 'r') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\ct.m3u8", 'r') as file:
          data = file.read()
          data = data.replace("playlist.m3u8","")
-        with open(r'ct.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\ct.m3u8",'w') as file:
 	        file.write(data)
         
         print("psText replaced")
@@ -84,32 +84,32 @@ for i in links:
         f.write('#EXTINF:-1 tvg-logo="https://github.com/arivarasan12/arivarasantv/blob/7fe542a265f3fef7ece784ed6e984c90d8a74909/all_serial/Images/vanthaipola.png?raw=true" group-title="SERIAL",VANATHA POLA"\n')
         f.write(i)
         f.write('\n')
-        urllib.request.urlretrieve(i,'vp.m3u8')
-        with open(r'vp.m3u8','r') as file:
+        urllib.request.urlretrieve(i,"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\vp.m3u8")
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\vp.m3u8",'r') as file:
          data = file.read()
          data = data.replace("fileSequence",i+"fileSequence")
-        with open(r'vp.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\vp.m3u8",'w') as file:
 	        file.write(data)
-        with open(r'vp.m3u8', 'r') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\vp.m3u8", 'r') as file:
          data = file.read()
          data = data.replace("playlist.m3u8","")
-        with open(r'vp.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\vp.m3u8",'w') as file:
 	        file.write(data)
         print("psText replaced")
       elif(substring == "pu"):
         f.write('#EXTINF:-1 tvg-logo="https://github.com/arivarasan12/arivarasantv/blob/db736b6ae6f1416cca65844856a5f60f7a5753e4/all_serial/Images/pooveunnakaga.png?raw=true" group-title="SERIAL",POOVE UNAKAGA\n')
         f.write(i)
         f.write('\n')
-        urllib.request.urlretrieve(i,'pu.m3u8')
-        with open(r'pu.m3u8', 'r') as file:
+        urllib.request.urlretrieve(i,"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\pu.m3u8")
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\pu.m3u8", 'r') as file:
           data = file.read()
           data = data.replace("fileSequence",i+"fileSequence")
-        with open(r'pu.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\pu.m3u8",'w') as file:
 	        file.write(data)
-        with open(r'pu.m3u8', 'r') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\pu.m3u8", 'r') as file:
          data = file.read()
          data = data.replace("playlist.m3u8","")
-        with open(r'pu.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\pu.m3u8",'w') as file:
 	        file.write(data)
         
         print("psText replaced")
@@ -117,31 +117,31 @@ for i in links:
         f.write('#EXTINF:-1 tvg-logo="https://www.tamildhool.net/wp-content/uploads/2021/10/kal.jpg" group-title="SERIAL",KAYAL\n')
         f.write(i)
         f.write('\n')
-        urllib.request.urlretrieve(i,'kay.m3u8')
-        with open(r'kay.m3u8','r') as file:
+        urllib.request.urlretrieve(i,"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\kay.m3u8")
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\kay.m3u8",'r') as file:
          data = file.read()
          data = data.replace("fileSequence",i+"fileSequence")
-        with open(r'kay.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\kay.m3u8",'w') as file:
 	        file.write(data)
-        with open(r'kay.m3u8', 'r') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\kay.m3u8", 'r') as file:
          data = file.read()
          data = data.replace("playlist.m3u8","")
-        with open(r'kay.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\kay.m3u8",'w') as file:
 	        file.write(data)
       elif(substring == keys[0]):
         f.write('#EXTINF:-1 tvg-logo="https://github.com/arivarasan12/arivarasantv/blob/87ac4e85cc497653eb5c62349e26cf8496259643/all_serial/Images/pandianstores.png?raw=true" group-title="SERIAL",PANDIAN STORES\n')
         f.write(i)
         f.write('\n')
-        urllib.request.urlretrieve(i,'ps.m3u8')
-        with open(r'ps.m3u8','r') as file:
+        urllib.request.urlretrieve(i,"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\ps.m3u8")
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\ps.m3u8",'r') as file:
          data = file.read()
          data = data.replace("fileSequence",i+"fileSequence")
-        with open(r'ps.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\ps.m3u8",'w') as file:
 	        file.write(data)
-        with open(r'ps.m3u8', 'r') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\ps.m3u8", 'r') as file:
          data = file.read()
          data = data.replace("playlist.m3u8","")
-        with open(r'ps.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\ps.m3u8",'w') as file:
 	        file.write(data)
         
         print("psText replaced")
@@ -150,16 +150,16 @@ for i in links:
         f.write(i)
         f.write(i)
         f.write('\n')
-        urllib.request.urlretrieve(i,'aru.m3u8')
-        with open(r'aru.m3u8','r') as file:
+        urllib.request.urlretrieve(i,"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\aru.m3u8")
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\aru.m3u8",'r') as file:
          data = file.read()
          data = data.replace("fileSequence",i+"fileSequence")
-        with open(r'aru.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\aru.m3u8",'w') as file:
 	        file.write(data)
-        with open(r'aru.m3u8', 'r') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\aru.m3u8", 'r') as file:
          data = file.read()
          data = data.replace("playlist.m3u8","")
-        with open(r'aru.m3u8','w') as file:
+        with open(r"C:\\Users\\ariva\\OneDrive\\Desktop\\TV\\arivarasantv\\aru.m3u8",'w') as file:
 	        file.write(data)
         
         print("psText replaced")
