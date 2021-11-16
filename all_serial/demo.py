b927303d244b107aee5da6b0d77a8064b0be9ad8
arivarasan12 <arivarasan254@gmail.com>
2021-11-16 04:35:28 +0000

    da

--- all_serial/demo.py
+++
@@ -1,6 +0,0 @@
-print("#EXTM3U")
-for i in range(0,1):
-    print("#EXTINF:-1 tvg-logo= \"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.facebook.com%2FRojaSerial%2Freviews%2F&psig=AOvVaw3ckpsJYdxgryixEiZH-oxQ&ust=1634724781948000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCLib9MKe1vMCFQAAAAAdAAAAABAJ\" group-title=\"SERIAL\",ரோஜா ",end="")
-    print("https://www.thiraisix.com/v/vijay/",end="")
-    print(i,end="")
-    print("10ps/720/playlist.m3u8"),
--- all_serial/mpddownload.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-for i in range (1,327):
-    print("https://suntvvod.s.llnwi.net/episodes/127277/hd/video/4/seg-",end="")
-    print(i,end="")
-    print(".m4s")
- #   https://suntvvod.s.llnwi.net/episodes/127277/hd/video/4/seg-1.m4s
